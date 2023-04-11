fd03e2e547f69a9853f1968085c25c14075d52cb
Daksh <dnakra@bsd220.org>
2023-04-11 09:31:15 -0500

    Initial Commit

---
+++ Cookie Clicker But Bad/Cookie_Clicker_But_BadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_Clicker_But_BadApp.swift
+//  Cookie Clicker But Bad
+//
+//  Created by Daksh Nakra on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_Clicker_But_BadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
