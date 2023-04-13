cc42dbdea3f24620c9f8450097c22568d80cef5b
Daksh <dnakra@bsd220.org>
2023-04-13 10:10:10 -0500

    Added the following:
    - App Icon
    - "Cook E Clicker" Title with Custom Font
    - A bad drawing of a cookie to click

--- Cookie Clicker But Bad/ContentView.swift
+++ Cookie Clicker But Bad/ContentView.swift
@@ -10,14 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Game is in development, please be paitent")
+            TitleText(text: "Cook E Clicker")
+            Image("Cook E").resizable().frame(width: 400, height: 400)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TitleText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Cochin", size: 48))
     }
 }
