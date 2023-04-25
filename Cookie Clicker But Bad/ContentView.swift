13127b428d715e88aae56c1c5c514eff1840f77c
Daksh <dnakra@bsd220.org>
2023-04-25 09:40:11 -0500

    Got rid of the test feature

--- Cookie Clicker But Bad/ContentView.swift
+++ Cookie Clicker But Bad/ContentView.swift
@@ -10,7 +10,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var test = 0.0
     @State private var placeNames =
     [
         "V",
@@ -35,16 +34,15 @@ struct ContentView: View {
         "m",
         "k"
     ]
-    @State private var cookies = 0
+    @State private var cookies = 0.0
     var body: some View {
         VStack {
             TitleText(text: "Cook E Clicker")
-            BodyText(text: "Cook E's Summoned: \(cookies)")
+            BodyText(text: "Cook E's Summoned: \(format(num: cookies))")
             Image("Cook E").resizable().frame(width: 400, height: 400)
                 .onTapGesture {
                     cookies += 1
                 }
-            Text(format(num: test))
         }
     }
     // This function formats the given number according to the digits place, ex: 10000000 -> "10m"
